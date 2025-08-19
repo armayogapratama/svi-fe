d85eed72109e8de6c2d39aeb0082915fb842d8f2
Arma Yoga Pratama <137013954+armayogapratama@users.noreply.github.com>
2025-08-20 03:05:55 +0700

    delete latest

--- sims-ppob-mgarmayogapratama/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./routers/routers";
-import "./App.css";
-
-export default function App() {
-  return (
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
-  );
-}
